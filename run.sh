0feb7beb9abc8606b812e7331052928ee41c3729
schnitz81 <k.quick81@gmail.com>
2018-06-10 06:05:30 +0200

    added starter script

---
+++ run.sh
@@ -0,0 +1,40 @@
+if ! [ -e beings ]; then
+	echo "No executable found. Preparing to build..."; sleep .6
+	echo -n "Checking gcc..."; sleep .4
+	if ! [ -e /usr/bin/gcc ]; then
+		echo -e "failed!\n\nMake sure gcc is installed.\n"
+		exit 1
+	fi
+	echo "success!"; sleep .4
+
+	echo -n "Checking curses..."; sleep .4
+	if ! [ -e /usr/include/curses.h ]; then
+		echo -e "failed!\n\ncurses.h not found. Make sure curses (most often ncurses) is installed.\n"
+		exit 1
+	fi
+	echo "success!"; sleep .4
+
+    echo "Proceeding to build executable..."; sleep .6
+
+	echo 'Building...'
+	gcc -Wall -c main.c
+	gcc -Wall -c actions.c
+	gcc -Wall -c cpu.c
+	gcc -Wall -c gameengine.c
+	gcc -Wall -c menus.c
+	gcc -o ttybattle main.o actions.o cpu.o gameengine.o menus.o -lcurses -lmenu
+
+	if [ $? -eq 0 ]; then
+		echo "Build successful. Starting executable..."; sleep 1
+		./ttybattle
+	else
+		echo "Build failed. Unable to start."
+	fi
+	
+else
+	echo -e "\nExecutable found. Starting..."; sleep 1
+	./ttybattle
+fi
+
+
+
